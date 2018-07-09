7c7f50fd8a6b497c476754217844207326da1c62
Alberto Melgar <alberto.melgar@ciberexperis.es>
2018-07-09 08:00:20 +0200

    Video 7 - Figuras geometricas circulos arcos lineas y poligonos

--- 02a.FIGURAS GEOMETRICAS - SIN RESOLVER/js/canvas.js
+++ 02a.FIGURAS GEOMETRICAS - SIN RESOLVER/js/canvas.js
@@ -9,3 +9,23 @@ ctx.lineWidth = 5;
 ctx.strokeStyle = "rgb(255,0,255,1)";
 ctx.rect(0,0,500,100);
 ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(300,300,80,0,2*Math.PI);
+ctx.fillStyle = "green";
+ctx.fill();
+
+ctx.strokeStyle = "red";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(200,200);
+ctx.lineTo(230,200);
+ctx.lineWidth = 5;
+ctx.strokeStyle = "yellow";
+ctx.stroke();
+
+ctx.fillStyle = "black";
+ctx.fill();
+
